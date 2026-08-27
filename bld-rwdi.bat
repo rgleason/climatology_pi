df015b26d943ca2f28244842788775bd7f7f3fdc
Rick Gleason <fcgleason@tgp-architects.com>
2026-08-27 15:15:44 -0400

    1.6.48.0

--- bld-rwdi.bat
+++ bld-rwdi.bat
@@ -7,15 +7,22 @@ REM  Using wxWidgets Release DLLs (/MD)
 REM  Full plugin debugging, no wxWidgets debugging required
 REM ============================================================
 
+
 echo.
-echo ===== CLEANING OLD BUILD =====
+echo ==== LOAD MSVC x86 ENV: cl.exe, link.exe, nmake, and the correct CRT ====
+echo.
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x86 -host_arch=x86
+cl
+
+echo ==== CLEANING OLD BUILD ====
 echo.
 
 rmdir /S /Q build 2>nul
 del CMakeCache.txt 2>nul
 
 echo.
-echo ===== CONFIGURING (RelWithDebInfo, /MD) =====
+echo ==== CONFIGURING (RelWithDebInfo, /MD) ====
 echo.
 
 cmake -A Win32 -T v143 ^
@@ -29,15 +36,38 @@ if errorlevel 1 (
 )
 
 echo.
-echo ===== BUILDING (RelWithDebInfo) =====
+echo ==== BUILDING (RelWithDebInfo) ====
+echo ==== Output sent to OUTPUT.txt ====
 echo.
 
-cmake --build build --config RelWithDebInfo
+cmake --build build --config RelWithDebInfo --verbose > OUTPUT.TXT
+
 
 if errorlevel 1 (
     echo BUILD FAILED
     exit /b 1
 )
+
+echo.
+echo ==== Copy DLL + PDB into OpenCPN build tree ====
+echo.
+
+copy /Y .\build\RelWithDebInfo\*_pi.dll C:\Users\fcgle\source\opencpn\build\RelWithDebInfo\plugins
+
+copy /Y .\build\RelWithDebInfo\*_pi.pdb C:\Users\fcgle\source\opencpn\build\RelWithDebInfo\plugins
+
+
+echo.
+echo ==== Build the tarball using CPACK ====
+echo.
+
+cmake --build build --config RelWithDebInfo --target package
+
+echo.
+echo ==== Run Cloudsmith upload script ====
+echo.
+
+call "%~dp0upload-cloudsmith.bat"
 
 echo.
 echo ============================================================
